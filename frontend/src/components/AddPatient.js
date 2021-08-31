2420907509f5bc990d3f8ad54c21c3988e03eda2
ahmedmahmoud116 <ahmedmahmoud116@ahmed.mahmoud8116@gmail.com>
2021-08-31 15:55:50 +0200

    first requests done

--- frontend/src/components/AddPatient.js
+++ frontend/src/components/AddPatient.js
@@ -1,19 +1,108 @@
-import React, { Component } from 'react';
-import { useParams } from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, useMutation } from '@apollo/client';
-import DoctorRow from './DoctorRow'
-import PatientRow from './PatientRow'
+import React, { useState } from 'react';
+import { gql, useMutation, useQuery} from '@apollo/client';
+import { Redirect } from 'react-router-dom';
 
-const putPatient = (props) => {
+const PutPatient = () => {
+    const FETCH_ALL_DOCTORS = gql`
+    query Doctor{
+      getAllDoctors {
+        firstName
+        lastName
+        specialization
+        id
+      }
+    }`;
 
-    const renderPatient = () => {
+    const ADD_NEW_PATIENT = gql`
+    mutation Mutation($addPatientFirstName: String!, $addPatientLastName: String!, 
+        $addPatientSyndrome: String, $addPatientDoctorId: String!) {
+        addPatient(firstName: $addPatientFirstName, lastName: $addPatientLastName, 
+            syndrome: $addPatientSyndrome, doctor_id: $addPatientDoctorId){
+                code
+                success
+                message
+                patient {
+                    id
+                    firstName
+                    lastName
+                    syndrome
+                    doctor_id
+                }
+            }
+        }
+    `;
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [syndrome, setSyndrome] = useState('');
+    const [doctorID, setDoctorID] = useState('');
+    const [redirect, setRedirect] = useState(false);
+
+
+    const { loading, error, data } = useQuery(FETCH_ALL_DOCTORS);
+    const [addDoctor, { datamut, loadingmut, errormut }] = useMutation(ADD_NEW_PATIENT);
+
+    if (loading) return <p> Loading... </p>
+    if (error) return <p> Error! </p>
+    const rows = [];
+    data.getAllDoctors.map((doctor) => {
+        rows.push(
+          <option value={doctor.id}>{doctor.firstName + " " + doctor.lastName}</option>
+        );
+    })    
+
+    if (loadingmut) return 'Submitting...'
+    if (errormut) return `Error! ${error.message}`
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addDoctor({ variables: { addPatientFirstName: firstName, addPatientLastName: lastName, addPatientSyndrome: syndrome, addPatientDoctorId: doctorID } });
+        setRedirect(true);
+    };
+
+    const patientForm = () => {
         return (
-            <div>
-                <h2>This is Add Patient!!</h2>
-            </div>
+            redirect ? <Redirect to='/AllPatients' /> :
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        First Name:
+                        <input
+                            type='text'
+                            value={firstName}
+                            onChange={e => setFirstName(e.target.value)}
+                        />
+                    </label>
+
+                    <label>
+                        Last Name:
+                        <input
+                            type='text'
+                            value={lastName}
+                            onChange={e => setLastName(e.target.value)}
+                        />
+                    </label>
+
+                    <label>
+                        Syndrome:
+                        <input
+                            type='text'
+                            value={syndrome}
+                            onChange={e => setSyndrome(e.target.value)}
+                        />
+                    </label>
+                    <select onChange={e => setDoctorID(e.target.value)} >
+                        {rows}
+                    </select>
+
+                    <input
+                        type='submit'
+                        value='Submit'
+                    />
+
+                </form>
         );
     };
-    return <div>{renderPatient()}</div>;
+    return <div>{patientForm()}</div>;
 }
 
-export default putPatient;
+export default PutPatient;
--- frontend/src/components/All_Patients.js
+++ frontend/src/components/All_Patients.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, useMutation } from '@apollo/client';
 import PatientRow from './PatientRow'
-
+import styled from "styled-components";
 
 const FETCH_ALL_PATIENTS = gql`
 query Patient {
@@ -28,20 +28,34 @@ function FetchAllPatients() {
     );
   })
 
+  const Button = styled.button`
+  background-color: #1e943d;
+  color: white;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+  `;
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Syndrome</th>
-          {/* <th>ID</th> */}
-          {/* <th>Doctor ID</th> */}
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Syndrome</th>
+            {/* <th>ID</th> */}
+            {/* <th>Doctor ID</th> */}
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+      <Button onClick={event => window.location.href = "addPatient/"}>
+        Add Patient
+      </Button>
+    </div>
   );
 
 }
